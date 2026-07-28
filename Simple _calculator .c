f8e9ed2600eb5a221c119ea19d695ae334d730c1
C.V.Suhas <cvsuhas10042009@gmail.com>
2026-07-29 04:34:30 +0530

    Enhance calculator with input prompts
    Added user prompts for operator and numbers input.

---
+++ Simple _calculator .c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+int main() {
+char operation;
+double n1,n2 ;
+  
+printf("enter the operator +,-,*,/");
+  
+scanf(" %c",&operation);
+  
+printf("Enter two numbers");
+  
+scanf("%lf %lf",&n1,&n2);
+switch(operation){
+    case '+':
+    printf("%.1lf + %.1lf = %.1lf",n1,n2,n1+n2);
+    break;
+    case '-':
+    printf("%.1lf - %.1lf = %.1lf",n1,n2,n1-n2);
+    break;
+    case '*':
+    printf("%.1lf * %.1lf = %.1lf",n1,n2,n1*n2);
+    break;
+    case '/':
+    if(n2 !=0){
+        
+    printf("%.1lf / %.1lf = %.1lf",n1,n2,n1/n2);
+    }
+    else{
+        printf("CAN'T DIVIDE BY ZERO");
+    }
+    break;
+    default:
+    printf("ERROR!");
+    
+}
+    return 0;
+}
