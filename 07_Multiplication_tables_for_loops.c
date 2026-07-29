1c5fe1059842aa26abd979f7c703ce7f569a2871
C.V.Suhas <cvsuhas10042009@gmail.com>
2026-07-30 04:12:56 +0530

    Enhance multiplication table program with user input
    Added user input for number and limit, and improved output formatting.

--- 07_Multiplication_tables_for_loops.c
+++ 07_Multiplication_tables_for_loops.c
@@ -3,12 +3,15 @@
 int main(){
 int n,limit;    
 int i;
+ 
 printf("ENTER YOUR NUMBER");
 scanf("%d",&n);
+ 
  printf("Enter your limit:");
 scanf("%d",&limit);
+ 
   for(i=0;i<=limit; i++){
- printf("%dx%d=%d\n",n,i,i*n);
+ printf("%d x %d =%d\n",n,i,i*n);
 }
 
     return 0;
