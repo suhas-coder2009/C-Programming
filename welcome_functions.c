8760f34c7fffc378e1aea35829f9ebe45cde52a6
C.V.Suhas <cvsuhas10042009@gmail.com>
2026-08-06 02:00:33 +0530

    Add welcome function and main program

---
+++ welcome_functions.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void welcome (){
+    printf("WELCOME TO FUNCTIONS IN C.\n");
+}
+
+int main() {
+welcome();
+welcome();
+welcome();
+    return 0;
+}
